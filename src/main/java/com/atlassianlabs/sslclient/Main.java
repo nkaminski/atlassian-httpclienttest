bee56d999d8b3e3af63124619c1bc705ab138d79
dcurrie@atlassian.com <dcurrie@atlassian.com>
2016-11-17 11:06:47 +1100

    Added logging for debugging.

--- src/main/java/com/atlassianlabs/sslclient/Main.java
+++ src/main/java/com/atlassianlabs/sslclient/Main.java
@@ -3,16 +3,16 @@ package com.atlassianlabs.sslclient;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Main.class);
 
         if (args.length != 1) {
             System.out.println("Usage: java -jar httpclienttest-1.0.jar <https-host-url>:<port>");
@@ -30,14 +30,14 @@ public class Main {
             throw new RuntimeException(e);
         }
         try {
-            BufferedReader rd = new BufferedReader
-                    (new InputStreamReader(response.getEntity().getContent()));
+            BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
         } catch (IOException e) {
             e.printStackTrace();
             throw new RuntimeException(e);
 
         }
 
+        System.out.println();
         System.out.println("HTTPClient: Connection Successful to https://" + args[0]);
 
     }
