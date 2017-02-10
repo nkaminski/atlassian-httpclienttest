43c4c4dd0d1df2751b141fb06b0ef851e5a8785c
dcurrie@atlassian.com <dcurrie@atlassian.com>
2017-02-10 15:25:43 +1100

    Modified httpclient that is constructed to use the same configuration that atlassian-gadgets use

--- src/main/java/com/atlassianlabs/sslclient/Main.java
+++ src/main/java/com/atlassianlabs/sslclient/Main.java
@@ -2,17 +2,25 @@ package com.atlassianlabs.sslclient;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.config.SocketConfig;
 import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.SystemDefaultRoutePlanner;
+import org.apache.http.message.BasicHeader;
 import org.apache.log4j.Logger;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ProxySelector;
+import java.util.Arrays;
 
 public class Main {
+    private static final int DEFAULT_CONNECT_TIMEOUT_MS = 5000;
+    private static final Logger LOGGER = Logger.getLogger(Main.class);
+
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(Main.class);
 
         if (args.length != 1) {
             System.out.println("Usage: java -jar httpclienttest-1.0.jar <https-host-url>:<port>");
@@ -20,7 +28,7 @@ public class Main {
             System.exit(1);
         }
 
-        HttpClient client = HttpClientBuilder.create().useSystemProperties().build();
+        HttpClient client = newHttpClient();
         HttpGet request = new HttpGet("https://" + args[0]);
         HttpResponse response = null;
         try {
@@ -39,6 +47,21 @@ public class Main {
 
         System.out.println();
         System.out.println("HTTPClient: Connection Successful to https://" + args[0]);
+    }
 
+    private static HttpClient newHttpClient() {
+        SocketConfig socketConfig = SocketConfig.custom().setSoTimeout(DEFAULT_CONNECT_TIMEOUT_MS).build();
+        HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+
+        SystemDefaultRoutePlanner routePlanner = new SystemDefaultRoutePlanner(ProxySelector.getDefault());
+        httpClientBuilder.setRoutePlanner(routePlanner);
+        httpClientBuilder.setDefaultSocketConfig(socketConfig);
+        httpClientBuilder.setDefaultHeaders((Arrays.asList(new BasicHeader("Accept-Encoding", "gzip, deflate"))));
+
+
+        RequestConfig.Builder requestConfigBuilder = RequestConfig.custom().setMaxRedirects(3);
+        httpClientBuilder.setDefaultRequestConfig(requestConfigBuilder.build());
+
+        return httpClientBuilder.build();
     }
 }
